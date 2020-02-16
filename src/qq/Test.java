ca3aadd5aa7d9cef67ddd404983c86666a3771f0
Ivan Pichurin <ivan.pichurin@gmail.com>
2020-02-16 12:16:06 +0300

    i--

--- src/qq/Test.java
+++ src/qq/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         for (int i = 1; i<=100;i++){
             System.out.print(" "+ i);
+            System.out.println("ssada");
         }
     }
 
